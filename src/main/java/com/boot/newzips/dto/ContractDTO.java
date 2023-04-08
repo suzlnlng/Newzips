0a988790d7fe95f68a4ce7f9b03e50f48f81b877
수진 <suzlnl@naver.com>
2023-04-08 15:58:56 +0900

    dto추가

---
+++ src/main/java/com/boot/newzips/dto/ContractDTO.java
@@ -0,0 +1,18 @@
+package com.boot.newzips.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ContractDTO {
+	
+	private String itemId;
+	private String userId;
+	private String startDate;
+	private String endDate;
+	private String temporaryContract;
+	private int tempContractCost;
+	private String contract;
+
+}
---
+++ src/main/java/com/boot/newzips/dto/MemberDTO.java
@@ -0,0 +1,22 @@
+package com.boot.newzips.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MemberDTO {
+	
+	private String userId;
+	private String userPwd;
+	private String userName;
+	private String userBirth;
+	private String userPhone;
+	private String userAge;
+	private String userAddr;
+	private String userEmail;
+	private String emailReceive;
+	private String marketing;
+	
+
+}
