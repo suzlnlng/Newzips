428b2abfff50dee77f3d3d18e5b6cf37857c8beb
555ohoh <coollnh@naver.com>
2023-04-09 18:08:10 +0900

    사용자 예약,계약 추가

--- src/main/java/com/boot/newzips/contract/ContractUserController.java
+++ src/main/java/com/boot/newzips/contract/ContractUserController.java
@@ -1,5 +1,15 @@
 package com.boot.newzips.contract;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ContractUserController {
+	
+	@GetMapping("newzips/contract_user")
+	public String contractUser() {
+		
+		return "/user/contract_user";
+	}
 
 }
--- src/main/java/com/boot/newzips/reservation/ReservationUserController.java
+++ src/main/java/com/boot/newzips/reservation/ReservationUserController.java
@@ -1,5 +1,54 @@
 package com.boot.newzips.reservation;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ReservationUserController {
+	
+	@GetMapping("newzips/reservation_user")
+	public String reservationUser() {
+		
+		return "/user/reservation_user";
+	}
+	
+	@GetMapping("newzips/reservation_resident")
+	public String reservationResident() {
+		
+		return "/user/reservation_resident";
+	}
+	
+	@GetMapping("newzips/reservation_user_complete")
+	public String reservationUserComplete() {
+		
+		return "/user/reservation_user_complete";
+	}
+	
+	
+	
+	
+	@GetMapping("newzips/reservation_user1")
+	public String user1() {
+		
+		return "/user/reservation_user1";
+	}
+
+	@GetMapping("newzips/reservation_resident1")
+	public String resident1() {
+		
+		return "/user/reservation_resident1";
+	}
+	
+	@GetMapping("newzips/reservation_user_complete1")
+	public String usercomplete1() {
+		
+		return "/user/reservation_user_complete1";
+	}
+	
+	@GetMapping("newzips/reservation_user_contract1")
+	public String usercontract1() {
+		
+		return "/user/reservation_user_contract1";
+	}
 
 }
