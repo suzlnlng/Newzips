0d7ee14260fcaebbcddce5b153fe336b36530268
yihnn <yihn95@naver.com>
2023-04-10 11:41:10 +0900

    add templates

--- src/main/java/com/boot/newzips/reservation/ReservationController.java
+++ src/main/java/com/boot/newzips/reservation/ReservationController.java
@@ -6,16 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ReservationController {
 
-	@GetMapping("/newzips/reservationStateRealtor")
-	public String reservationStateRealtor(){
-		
-		return "reservationStateRealtor";
-	}
-	
-	@GetMapping("/newzips/reservationListRealtor")
-	public String reservationListRealtor(){
-		
-		return "reservationListRealtor";
-	}
+
 	
 }
