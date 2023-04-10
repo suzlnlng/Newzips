7a45d0faf2e9fc31a4f14c52825d4a8b73c0755d
수진 <suzlnl@naver.com>
2023-04-10 12:12:28 +0900

    HtmlController 추가

--- src/main/java/com/boot/newzips/main/MainUserController.java
+++ src/main/java/com/boot/newzips/main/MainUserController.java
@@ -1,5 +1,36 @@
 package com.boot.newzips.main;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
 public class MainUserController {
+	
+	@GetMapping("/newzips")
+	public ModelAndView main() throws Exception{
+		
+		ModelAndView mav = new ModelAndView();
+		
+		mav.setViewName("user/home_user");
+
+		return mav;
+		
+	}
+	
+
+	
+	
+	
+	@GetMapping("/login_test")
+	public String login_test() {
+		
+		return "user/404";
+	}
+	
+	
 
 }
