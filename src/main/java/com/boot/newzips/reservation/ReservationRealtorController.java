51c172c0b4d0b7e3e5bc3422ae16a3516a78325b
yihnn <yihn95@naver.com>
2023-04-12 12:07:22 +0900

    db 구축완료

--- src/main/java/com/boot/newzips/reservation/ReservationRealtorController.java
+++ src/main/java/com/boot/newzips/reservation/ReservationRealtorController.java
@@ -6,6 +6,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ReservationRealtorController {
 
+	@GetMapping("/newzips/reservationRequestRealtor")
+	public String reservationRequestRealtor() {
+		
+		return "realtor/reservationRequestRealtor";
+	}
+
+	@GetMapping("/newzips/reservationStateRealtor")
+	public String reservationStateRealtor() {
+		
+		return "realtor/reservationStateRealtor";
+	}
 
 }
 
