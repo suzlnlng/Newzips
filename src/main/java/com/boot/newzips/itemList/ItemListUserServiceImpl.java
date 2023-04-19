49a5583dfebac24afe80943a11d537d8fc74ce16
ihwan1 <hwani9284@gmail.com>
2023-04-19 10:55:02 +0900

    리스트띄워보기

--- src/main/java/com/boot/newzips/itemList/ItemListUserServiceImpl.java
+++ src/main/java/com/boot/newzips/itemList/ItemListUserServiceImpl.java
@@ -1,27 +1,41 @@
 package com.boot.newzips.itemList;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.boot.newzips.dto.JunsaeListingDTO;
+import com.boot.newzips.dto.ListAllDTO;
 import com.boot.newzips.dto.ListingDTO;
+import com.boot.newzips.dto.WolseListingDTO;
 
+@Service
 public class ItemListUserServiceImpl implements itemListUserService {
-	
-	@Autowired
-	private ItemListUserMapper itemListUserMapper;
-	
-	public List<ListingDTO> selectListingDTO() {
-	    
-	    List<ListingDTO> listingDTOList = new ArrayList<>(); //빈 배열 객체를 만들고 listingDTOList에 집어넣음
-	    List<ListingDTO> listingList = itemListUserMapper.selectListingDTO(); //selectListingDTO를 통해 조회된 값들이  List<ListingDTO>형태로 반환되고 listingList에 들어간다
-	    for (ListingDTO listingDTO : listingList) {  //listingList안에 있는 listingDTO로 반복문을 돌려서 convertedListingDTO에 넣는다
-	        ListingDTO convertedListingDTO = new ListingDTO();
-	     
-	        // ...
-	        listingDTOList.add(convertedListingDTO); //convertedListingDTO를 아까만들어둔 빈객체(listingDTOList)에 넣는다
-	    }
-	    return listingDTOList;
-	}
+
+    @Autowired
+    private ItemListUserMapper itemListUserMapper;
+
+    @Override
+    public List<ListingDTO> YMListingDTO(String itemId) throws Exception {
+        return itemListUserMapper.YMListingDTO(itemId);
+    }
+
+    @Override
+    public List<WolseListingDTO> getread_wolse(String itemId) throws Exception {
+        return itemListUserMapper.getread_wolse(itemId);
+    }
+
+    @Override
+    public List<JunsaeListingDTO> getread_junsae(String itemId) throws Exception {
+        return itemListUserMapper.getread_junsae(itemId);
+    }
+    @Override
+    public List<ListAllDTO> getreadDataAll() throws Exception {
+        return itemListUserMapper.getreadDataAll();
+    }
+    @Override
+    public List<ListAllDTO> getreadDatapage() throws Exception {
+        return itemListUserMapper.getreadDatapage();
+    }
 }
--- src/main/java/com/boot/newzips/itemList/itemListUserService.java
+++ src/main/java/com/boot/newzips/itemList/itemListUserService.java
@@ -1,5 +1,21 @@
 package com.boot.newzips.itemList;
+import java.util.List;
+
+import com.boot.newzips.dto.JunsaeListingDTO;
+import com.boot.newzips.dto.ListAllDTO;
+import com.boot.newzips.dto.ListingDTO;
+
+import com.boot.newzips.dto.WolseListingDTO;
 
 public interface itemListUserService {
 
+    public List<ListingDTO> YMListingDTO(String itemId) throws Exception;
+
+    public List<WolseListingDTO> getread_wolse(String itemId) throws Exception;
+
+    public List<JunsaeListingDTO> getread_junsae(String itemId) throws Exception;
+    
+    public List<ListAllDTO> getreadDataAll() throws Exception;
+    
+    public List<ListAllDTO> getreadDatapage() throws Exception;
 }
