3fff09f77314b639df28a9de11202f6bde6ea3c8
555ohoh <coollnh@naver.com>
2023-04-27 13:49:53 +0900

    ㅇㅇ

--- src/main/java/com/boot/newzips/contract/ContractUserController.java
+++ src/main/java/com/boot/newzips/contract/ContractUserController.java
@@ -7,35 +7,50 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.boot.newzips.dto.JunsaeListingDTO;
 import com.boot.newzips.dto.ListingDTO;
-import com.boot.newzips.itemDetail.ItemDetailService;
-import com.boot.newzips.itemList.itemListUserService;
-import com.boot.newzips.service.ContractUserService;
+import com.boot.newzips.dto.MemberDTO;
+import com.boot.newzips.dto.WolseListingDTO;
+import com.boot.newzips.service.contractUSerService;
+
+
 
 @Controller
 public class ContractUserController {
 	
-	
 	@Resource
-	private ContractUserService contractUserService;
+	private contractUSerService contractUserService;
 	
 	@GetMapping("newzips/contract_user")
 	public ModelAndView contractUser(HttpServletRequest request) throws Exception {
 		
-//		String userId = request.getParameter("userId");
-//		userId = "karina";
+		
+		String userId = request.getParameter("userId");
+		userId = "suzi";
 		
 		String itemId = request.getParameter("itemId");
-		itemId = "35738667";
+		itemId = "2396640";
 		
 		ModelAndView mav = new ModelAndView();
-		
-		ListingDTO dtoL = contractUserService.getListing(itemId);
 
+		MemberDTO member = contractUserService.getUser(userId);
+		ListingDTO listing = contractUserService.getListing(itemId);
+		WolseListingDTO wolse = contractUserService.getWolse(itemId);
+		JunsaeListingDTO junsae = contractUserService.getJunsae(itemId);
+		
+		mav.addObject("member",member);
+		mav.addObject("listing",listing);
+		mav.addObject("wolse",wolse);
+		mav.addObject("junsae",junsae);
+		
+		System.out.println(member);
+		
+		System.out.println("출력해라");
 		
 		mav.setViewName("/user/reservation_user_contract1");
 		
 		return mav;
+		
 	}
 	
 
